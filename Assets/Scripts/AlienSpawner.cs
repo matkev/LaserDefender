2b6e03bf2cd485da1d1af2256e217d6773b382e7
Matthew Kevork <matthew.kevork@hotmail.com>
2018-05-21 23:56:42 -0400

    formation reappears one by one after being defeated

--- Assets/Scripts/AlienSpawner.cs
+++ Assets/Scripts/AlienSpawner.cs
@@ -7,7 +7,7 @@ public class AlienSpawner : MonoBehaviour {
     public float width = 10f;
     public float height = 5f;
     public float speed = 1f;
-
+    public float spawnDelay = 0.5f;
 
     public float padding = 0;
 
@@ -19,7 +19,7 @@ public class AlienSpawner : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-        
+
         float distance = transform.position.z - Camera.main.transform.position.z;
         Vector3 leftEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
         Vector3 rightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
@@ -33,13 +33,7 @@ public class AlienSpawner : MonoBehaviour {
         xmin = leftEdge.x;
         xmax = rightEdge.x;
 
-        
-        foreach (Transform child in transform)
-        {
-            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
-            enemy.transform.parent = child;
-	
-        }
+        SpawnUntilFull();
 	}
 	
 
@@ -83,12 +77,64 @@ public class AlienSpawner : MonoBehaviour {
         }
         
 
+        if (AllMembersDead())
+        {
+            print("Empty formation");
+            SpawnUntilFull();
+        }
 
+       
+    }
 
+    bool AllMembersDead()
+    {
+        foreach(Transform childPositionGameObject in transform)
+        {
+            if (childPositionGameObject.childCount > 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
-        //float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+    Transform NextFreePosition()
+    {
+        foreach (Transform childPositionGameObject in transform)
+        {
+            if (childPositionGameObject.childCount == 0)
+            {
+                return childPositionGameObject;
+            }
+        }
+        return null;
+    }
 
-        //transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    void SpawnFormation()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+
+        }
+    }
+
+    void SpawnUntilFull()
+    {
+        Transform freePosition = NextFreePosition();
+        if (freePosition)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, freePosition.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = freePosition;
+
+        }
+        if (NextFreePosition())
+        {
+            Invoke("SpawnUntilFull", spawnDelay);
+
+        }
+
 
     }
 }
