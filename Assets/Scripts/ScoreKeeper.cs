94ac43558a907ec63c29029d7f68e5840c24f101
Matthew Kevork <matthew.kevork@hotmail.com>
2018-05-24 13:44:04 -0400

    added score keeper UI

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public int score = 0;
+    private Text myText;
+
+
+
+    public void Score(int points)
+    {
+        this.score += points;
+        myText.text = score.ToString();
+    }
+
+
+    public void Reset()
+    {
+        this.score = 0;
+        myText.text = score.ToString();
+
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+        myText = GetComponent<Text>();
+        Reset();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
